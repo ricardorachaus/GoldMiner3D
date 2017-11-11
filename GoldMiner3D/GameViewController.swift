da30428079a1c220b6e0f417c2d4165fea7c5e1c
Ricardo Rachaus <ricardorachaus@gmail.com>
2017-11-11 15:21:25 -0200

    Handles tap, get object and destroy

--- GoldMiner3D/GameViewController.swift
+++ GoldMiner3D/GameViewController.swift
@@ -12,115 +12,83 @@ import SceneKit
 
 class GameViewController: UIViewController {
 
+    var claw: SCNNode?
+    var movementAction: SCNAction!
+    
+    let movementKey = "movement"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
+        let scene = SCNScene(named: "art.scnassets/GameScene.scn")!
         let scnView = self.view as! SCNView
         
-        // set the scene to the view
         scnView.scene = scene
-        
-        // allows the user to manipulate the camera
         scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
         scnView.showsStatistics = true
         
-        // configure the view
-        scnView.backgroundColor = UIColor.black
+        claw = scene.rootNode.childNode(withName: "Claw", recursively: false)
         
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
+        let action = SCNAction.move(by: SCNVector3(0, 0, -4), duration: 1)
+        let reverse = action.reversed()
+        let initialPosition = SCNAction.move(to: SCNVector3(0, 0, 0), duration: 1)
+        let sequence = SCNAction.sequence([action, initialPosition, reverse, initialPosition])
+        movementAction = SCNAction.repeatForever(sequence)
+
+        claw?.runAction(movementAction, forKey: movementKey)
+        
+        scene.physicsWorld.contactDelegate = self
+        
     }
     
-    @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let _ = touches.first!
         
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result = hitResults[0]
+        claw?.removeAction(forKey: movementKey)
+        claw?.runAction(SCNAction.move(by: SCNVector3(0, -10, 0), duration: 1))
+        claw?.runAction(SCNAction.move(by: SCNVector3(0, 0, 0), duration: 1))
+    }
+    
+}
+
+extension GameViewController: SCNPhysicsContactDelegate {
+    
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        let nodeA = contact.nodeA
+        let nodeB = contact.nodeB
+        let name = nodeA.name!
+        
+        if nodeB.name == "Claw" {
+            nodeB.removeAllActions()
+            nodeB.addChildNode(nodeA)
             
-            // get its material
-            let material = result.node.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
+            let action = SCNAction.move(to: SCNVector3(nodeB.position.x, 0, nodeB.position.z), duration: 2)
+            let block = SCNAction.run { (node) in
+                let object = node.childNode(withName: name, recursively: false)
+                object?.removeFromParentNode()
             }
             
-            material.emission.contents = UIColor.red
+            let sequence = SCNAction.sequence([action, block])
             
-            SCNTransaction.commit()
+            nodeB.runAction(sequence)
+            nodeB.runAction(movementAction, forKey: movementKey)
+        }
+        else {
+            nodeB.removeAllActions()
+            nodeB.addChildNode(nodeA)
+            
+            let action = SCNAction.move(to: SCNVector3(nodeB.position.x, 0, nodeB.position.z), duration: 2)
+            let block = SCNAction.run { (node) in
+                let object = node.childNode(withName: name, recursively: false)
+                object?.removeFromParentNode()
+            }
+            
+            let sequence = SCNAction.sequence([action, block])
+            
+            nodeB.runAction(sequence)
+            nodeB.runAction(movementAction, forKey: movementKey)
         }
     }
     
-    override var shouldAutorotate: Bool {
-        return true
-    }
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
 }
